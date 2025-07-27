5409adbbd93b2fc2cb418d47f947679af65decda
Mina-107 <mn.magdy455@gmail.com>
2025-07-27 18:28:17 +0200

    Create GlobalUsing.cs

---
+++ DataAccessLayer/GlobalUsing.cs
@@ -0,0 +1,23 @@
+
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using DataAccessLayer.Data;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Linq.Expressions;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using DataAccessLayer.Data.Entities;
+global using Domain.Entities;
+global using Domain.Entities.OrderEntities;
+global using DataAccessLayer.Repository;
+global using Microsoft.EntityFrameworkCore.Storage;
+global using Domain.Contracts;
+global using StackExchange.Redis;
+global using System.Text.Json;
+global using Order = Domain.Entities.OrderEntities.Order;
+global using System.Collections.Concurrent;
+global using Persistence.Repositories;
+
+
+
