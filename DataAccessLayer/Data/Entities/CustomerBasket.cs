60f7488c8ed1f123b53f75751ece651573fc4bf9
Mina-107 <mn.magdy455@gmail.com>
2025-07-27 18:23:15 +0200

    Create CustomerBasket.cs

---
+++ DataAccessLayer/Data/Entities/CustomerBasket.cs
@@ -0,0 +1,17 @@
+
+namespace DataAccessLayer.Data.Entities
+
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; }
+        public IEnumerable<BasketItem> Items { get; set; }
+         public string? PaymentIntentId { get; set; }
+         public string? ClientSecret { get; set; }
+         public int?DeliveryMethodId { get; set; }
+         public decimal? ShippingPrice { get; set; }
+
+
+
+    }
+}
